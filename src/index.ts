bca7b886e296479243d5192ce427488fde4f2feb
yang19970310 <348989238@qq.com>
2020-11-29 00:51:52 +0800

    new

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
-import { ArrayHelper } from "./ArrayHelper";
-
-//函数使用泛型
-function tack<T>(arr: T[], n: number): T[] {
-    if (n >= arr.length) {
-        return arr;
-    }
-    const newArr: T[] = [];
-    for (let i = 0; i < n; i++) {
-        newArr.push(arr[i])
-    }
-    return newArr;
+//泛型约束
+/**
+ * 将某个对象的name属性的每个单词的首字母大写，然后返回
+ */
+interface hasNameProperty{
+    name:string
 }
-const newArr = tack(['1', '2', '3', 'a4', '5', '6'], 4)
-console.log(newArr)
-//类型别名使用泛型
-type callback<T> = (n: T, i: number) => boolean
-function filter<T>(arr:T[], callback: callback<T>): T[] {
-    const newArr: T[] = [];
-    
-    return newArr;
+function nameToUpperCase<T extends hasNameProperty>(obj :T):T{
+    obj.name = obj.name.split(" ").map(s=>s[0].toUpperCase() + s.substr(1)).join(" ")
+    return obj;
 }
-const arr = [3, 5, 6, 8, 7];
-console.log(filter(arr, (n,i) => (i) % 2 !== 0))
-//类中使用泛型
-let arr1 = new ArrayHelper(["aa","cc","ff","bb","dd"])
-console.log(arr1.shuffle())
+const o = {
+    name : "yang jian",
+    age:20
+}
+const newO = nameToUpperCase(o);
+console.log(newO)
