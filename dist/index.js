f0dfd43216d5c3896a730ccd7447fadfd11400be
yang19970310 <348989238@qq.com>
2020-12-05 18:30:12 +0800

    this指向

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,10 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-const animals_1 = require("./animals");
-const animals = [
-    new animals_1.Lion("小狮子", 44),
-    new animals_1.Tiger("小老虎", 11),
-    new animals_1.Dog("小二哈", 4),
-    new animals_1.Monkey("小猴子", 4)
-];
-function hasFireShow(ani) {
-    if (ani.singleFire && ani.doubleFire) {
-        return true;
+class user {
+    constructor(name) {
+        this.name = name;
     }
-    return false;
+    sayHello() {
+        console.log(this, this.name);
+    }
 }
-animals.forEach(ele => {
-    if (hasFireShow(ele)) {
-        ele.singleFire();
-        ele.doubleFire();
-    }
-    else {
-        ele.syHolle();
-    }
-});
+const un = new user('name');
+const h = un.sayHello;
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,21 @@
-
-import { unlink } from "fs";
-import { Dog, Lion, Monkey, Tiger,Animal} from "./animals";
-import { IFireShow, IWisdomShow } from "./interfaces";
-
-
-const animals:Animal[]=[
-    new Lion("小狮子",44),
-    new Tiger("小老虎",11),
-    new Dog("小二哈",4),
-    new Monkey("小猴子",4)
-]
-function hasFireShow(ani: object):ani is IFireShow{
-    if((ani as unknown as IFireShow).singleFire && (ani as unknown as IFireShow).doubleFire){
-        return true;
-    }
-    return false;
-    
-}
-// animals.forEach(ele=>{
-//     if(hasFireShow(ele)){
-//         ele.singleFire()
-//         ele.doubleFire()
-//     }else{
-//         ele.syHolle()
+// const u = {
+//     name:"afsa",
+//     sayHello(){
+       
+//         console.log(this,this.name)
 //     }
-// })
-function  hasIWisdomShow(ele:object):ele is  IWisdomShow{
-    if((ele as unknown as IWisdomShow).suanshuti && (ele as unknown as IWisdomShow).tiaowu){
-        return true
-    }else{
-        return false
-    }
-    
-}
-animals.forEach(ele=>{
-    if(hasIWisdomShow(ele)){
-        ele.suanshuti()
-        ele.tiaowu()
-    }else{
-        ele.syHolle()
-    }
-})
+// }
+// const say = u.sayHello;
+//say()
+ //this指向any
 
-class A{
-    a:string
-    b:string
-}
-class B{
-    c:string
-    d:string
-}
-interface c extends A,B{}
-const C:c ={
-    a:'',
-    b:'',
-    c:'',
-    d:''
-}
-console.log(typeof C)
+ class user{
+     constructor(public name){
+
+     }
+     sayHello(this:user){
+         console.log(this,this.name)
+     }
+ }
+ const un= new user('name')
+const h = un.sayHello;
